ed443049361f5de88abbf55999d934886ee8c079
Adeyemi Tolulope <tolulopepaul2004@gmail.com>
2023-08-11 17:58:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Successful only when 0
+ */
+
+int main(void)
+{
+	char c;
+	
+	char w;
+
+	c = 'a';
+	w = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(w <= 'z') {
+			putchar (w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
