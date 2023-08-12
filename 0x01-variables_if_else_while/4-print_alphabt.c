da7c9ef70ccb9abd23a0582b0eb1f5fc683d027b
Adeyemi Tolulope <tolulopepaul2004@gmail.com>
2023-08-11 18:29:33 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: Succssful only when 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
