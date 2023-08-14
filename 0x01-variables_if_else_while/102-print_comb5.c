9b026c00994124566ddfa6c1323a19c35f8f7944
Adeyemi Tolulope <tolulopepaul2004@gmail.com>
2023-08-14 04:37:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,30 +3,31 @@
 
 /**
  * main - Entry Function
- *
- * Return: successful when 0
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; j++)
+	for (i = 0; i < 100; i++)
 	{
-		if (i < j)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 99)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
-}
