62054bdf04c48cd626a3604ac96de175ccf946e0
Adeyemi Tolulope <tolulopepaul2004@gmail.com>
2023-08-21 14:43:06 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int nb;
 
+
 	nb = _atoi("98");
 	printf("%d\n", nb);
 	nb = _atoi("-402");
@@ -22,10 +23,10 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi("         +      +    -    -98 Battery Street;
-	San Francisco, CA 94111 - USA             ");
+	nb = _atoi('         +      +    -    -98 Battery Street; San Francisco
+	CA 94111 - USA             ');
 	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	nb = _atoi('---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
 	return (0);
 }
